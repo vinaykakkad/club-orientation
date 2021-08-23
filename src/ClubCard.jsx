56309bcca6b989e19453024b36e1cc9a2ece8c65
Rohan Parikh <rohanparikh.72@gmail.com>
2021-08-24 01:20:36 +0530

    redirecting to insta page of the club when the respective card is clicked upon

--- src/ClubCard.jsx
+++ src/ClubCard.jsx
@@ -4,50 +4,54 @@ import './card.css'
 const {Meta} = Card;
 const {Text} = Typography;
 function ClubCard(props) {
-    const click = () => {
-        message.success(`${props.club}\'s Instagram handle copied to the clipboard!`);
-        navigator.clipboard.writeText(props.insta);
-    }
+    // const click = () => {
+    //     message.success(`${props.club}\'s Instagram handle copied to the clipboard!`);
+    //     navigator.clipboard.writeText(props.insta);
+    // }
     return (
-        <Col
-            onClick={click}
-            className="clubcard"
-            style={{
-                width: '95%',
-                cursor: 'pointer',
-                border: '2px solid #e5e5e5',
-                padding: '10px',
-                borderRadius: '8px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backdropFilter: 'blur(5px)',
-                height: '150px'
-            }}
-        >
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center'  
-            }}>
-                <Text style={{
-                    textAlign: 'center' ,
-                    height: 'fit-content',
+            <Col
+                // onClick={click}
+                className="clubcard"
+                style={{
+                    width: '95%',
+                    cursor: 'pointer',
+                    border: '2px solid #e5e5e5',
+                    padding: '10px',
+                    borderRadius: '8px',
                     display: 'flex',
+                    justifyContent: 'center',
                     alignItems: 'center',
-                    fontSize: '18px',
-                    fontWeight: '500'
+                    backdropFilter: 'blur(5px)',
+                    height: '150px'
+                }}
+            >
+                <a target="_blank" rel="noreferrer" href={`https://www.instagram.com/${props.insta}/`}> 
+
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'  
                 }}>
-                    {props.club}
-                </Text>
-                <Divider style={{ marginTop: '10px', marginBottom: '10px', borderColor: '#fff'}}/>
-                <Text style={{margin: ''}}>
-                    @{props.insta}
-                </Text>
-                
-            </div>
-        </Col>
+                    <Text style={{
+                        textAlign: 'center' ,
+                        height: 'fit-content',
+                        display: 'flex',
+                        alignItems: 'center',
+                        fontSize: '18px',
+                        fontWeight: '500'
+                    }}>
+                        {props.club}
+                    </Text>
+                    <Divider style={{ marginTop: '10px', marginBottom: '10px', borderColor: '#fff'}}/>
+                    <Text style={{margin: ''}}>
+                        @{props.insta}
+                    </Text>
+                    
+                </div>
+                </a>
+            </Col>
+        
     );
 }
 
