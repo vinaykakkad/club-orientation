a7398d6e1b7fc568ba40fc87d3f2e72fea24b0ee
Vinay Kakkad <kakkadvinay25@gmail.com>
2021-08-24 03:21:21 +0000

    refactor, update background and font style

--- src/ClubCard.jsx
+++ src/ClubCard.jsx
@@ -1,58 +1,64 @@
-import React from 'react';
-import { Card, Row, Col, message, Button, Divider, Typography } from 'antd';
-import './card.css'
-const {Meta} = Card;
-const {Text} = Typography;
-function ClubCard(props) {
-    // const click = () => {
-    //     message.success(`${props.club}\'s Instagram handle copied to the clipboard!`);
-    //     navigator.clipboard.writeText(props.insta);
-    // }
-    return (
-            <Col
-                // onClick={click}
-                className="clubcard"
-                style={{
-                    width: '95%',
-                    cursor: 'pointer',
-                    border: '2px solid #e5e5e5',
-                    padding: '10px',
-                    borderRadius: '8px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backdropFilter: 'blur(5px)',
-                    height: '150px'
-                }}
-            >
-                <a target="_blank" rel="noreferrer" href={`https://www.instagram.com/${props.insta}/`}> 
+import React from "react";
+import { Col, Divider, Typography } from "antd";
+import "./card.css";
 
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'  
-                }}>
-                    <Text style={{
-                        textAlign: 'center' ,
-                        height: 'fit-content',
-                        display: 'flex',
-                        alignItems: 'center',
-                        fontSize: '18px',
-                        fontWeight: '500'
-                    }}>
-                        {props.club}
-                    </Text>
-                    <Divider style={{ marginTop: '10px', marginBottom: '10px', borderColor: '#fff'}}/>
-                    <Text style={{margin: ''}}>
-                        @{props.insta}
-                    </Text>
-                    
-                </div>
-                </a>
-            </Col>
-        
-    );
+const { Text } = Typography;
+
+function ClubCard(props) {
+  return (
+    <Col
+      className="clubcard"
+      style={{
+        width: "95%",
+        cursor: "pointer",
+        border: "2px solid gray",
+        padding: "10px",
+        borderRadius: "8px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backdropFilter: "blur(5px)",
+        height: "150px",
+      }}
+    >
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`https://www.instagram.com/${props.insta}/`}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              height: "fit-content",
+              display: "flex",
+              alignItems: "center",
+              fontSize: "18px",
+              fontWeight: "500",
+              color: "whitesmoke"
+            }}
+          >
+            {props.club}
+          </Text>
+          <Divider
+            style={{
+              marginTop: "10px",
+              marginBottom: "10px",
+              borderColor: "#fff",
+            }}
+          />
+          <Text style={{ margin: "", color: "#f5f5f5" }}>@{props.insta}</Text>
+        </div>
+      </a>
+    </Col>
+  );
 }
 
 export default ClubCard;
